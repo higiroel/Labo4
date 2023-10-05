8dd40cb343d3315f4a6548b7644788d94ab3ae71
higiroel <146278898+higiroel@users.noreply.github.com>
2023-10-05 14:01:52 -0400

    Initial commit

---
+++ src/FortuneTellerViewer.java
@@ -0,0 +1,8 @@
+
+public class FortuneTellerViewer {
+    public static void main(String[] args) {
+        // Create and show the JFrame
+        FortuneTellerFrame frame = new FortuneTellerFrame();
+        frame.setVisible(true);
+    }
+}
